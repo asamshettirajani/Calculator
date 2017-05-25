0d88222d3b5050a123f4ce330087cd934dc82b90
istar <istar@192.168.100.23>
2017-05-25 10:27:40 +0530

    program for print the name

---
+++ src/com/onlinecalculator/calculator/calctypes/string3.java
@@ -0,0 +1,14 @@
+package com.onlinecalculator.calculator.calctypes;
+
+import java.util.Scanner;
+
+public class string3 {
+	public static void main(String[] args) {
+		{
+		
+			String name;
+			Scanner sc=new Scanner(System.in);
+			System.out.println("rajani");
+}
+}
+}
