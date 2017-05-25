d86c7fc9c203f923a5922b07564d23f0dcc86227
istar <istar@192.168.100.23>
2017-05-25 10:24:08 +0530

    program for dispaly the name

---
+++ src/com/onlinecalculator/calculator/calctypes/String1.java
@@ -0,0 +1,23 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.*;
+
+public class String1 {
+
+	public static void main(String[] args) {
+		{
+			double a,b;
+			String name;
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter your name:");
+			name=sc.nextLine();
+			System.out.println("welcome to java");
+			System.out.println("enter first number");
+			a=sc.nextDouble();
+			System.out.println("enter second number");
+			b=sc.nextDouble();
+			System.out.println("addition:"+(a+b));
+			
+		}
+	}
+
+}
