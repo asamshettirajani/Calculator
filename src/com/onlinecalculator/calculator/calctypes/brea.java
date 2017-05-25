f64e1eca918ef8c155ad5a01c2a80360c66eed16
istar <istar@192.168.100.23>
2017-05-25 10:13:35 +0530

    break statement program

---
+++ src/com/onlinecalculator/calculator/calctypes/brea.java
@@ -0,0 +1,13 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.*;
+public class brea {
+	public static void main(String args[])
+
+	{
+for(int x=1;x<30;x++){
+	System.out.println(x);
+	if(x==20)
+		break;
+}
+	}
+}
